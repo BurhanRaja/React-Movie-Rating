9c7ece124da2fd6b3515b54372f5302cc920d5c5
BurhanuddinRaja <mails4burhan@gmail.com>
2022-07-09 20:26:06 +0530

    Header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,17 @@ import React from 'react'
 const Header = () => {
     return (
         <div>
-            <nav className="nav-container flex flex-row p-3">
-                <div className="webname-container">
-                    <img src="/assets/logo.svg" alt="Logo" className='webname w-4 h-auto' />
+            <nav className="nav-container flex flex-row p-4" style={{"backgroundColor":"#031e40"}}>
+                <div className="webname-container pl-6">
+                    <a href="">
+                    <img src="/assets/logo.svg" alt="Logo" className='webname w-36 h-auto' />
+                    </a>
                 </div>
-                <ul className="navItem-container flex flex-row justify-center">
-                    <li className="nav-item"><a href="#">Movies</a></li>
-                    <li className="nav-item"><a href="#">TV Shows</a></li>
-                    <li className="nav-item"><a href="#">People</a></li>
+                <ul className="navItem-container flex flex-row justify-center mx-3 font-semibold font-sans text-sm text-white">
+                    <li className="nav-item mx-3"><a className='hover:text-sky-100' href="#">Movies</a></li>
+                    <li className="nav-item mx-3"><a className='hover:text-sky-100' href="#">TV Shows</a></li>
+                    <li className="nav-item mx-3"><a className='hover:text-sky-100' href="#">People</a></li>
+                    <li className="nav-item mx-3"><a className='hover:text-sky-100' href="#">More</a></li>
                 </ul>
             </nav>
         </div>
